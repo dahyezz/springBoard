227acb5c90d2e2ce892e934c46aded9768867aa4
dahyezz <anwlrqlc@naver.com>
2019-07-10 14:14:10 +0900

    ignore test

--- SpringBoard/src/main/java/web/controller/BoardController.java
+++ SpringBoard/src/main/java/web/controller/BoardController.java
@@ -24,7 +24,6 @@ public class BoardController {
 	@RequestMapping(value="/board/list", method=RequestMethod.GET)
 	public void getList(Model model, String curPage) {
  
-
 		Paging paging = boardService.getcurPage(curPage);
 		
 		List<Board> boardList = boardService.list(paging);
